74ee576522b9093abc1a1161495bad851a71163c
Piotr Tomiak <piotr@genuitec.com>
2014-10-07 14:18:16 +0200

    [nobug] Categorized content assist.
    Change-Id: Ifda71f2c763c80994463a6c4a3bad97966476edb

--- eclipse/tern.eclipse.ide.jsdt/src/tern/eclipse/ide/jsdt/internal/contentassist/JSDTTernCompletionProposal.java
+++ eclipse/tern.eclipse.ide.jsdt/src/tern/eclipse/ide/jsdt/internal/contentassist/JSDTTernCompletionProposal.java
@@ -23,6 +23,15 @@ import tern.eclipse.ide.ui.contentassist.JSTernCompletionProposal;
 public class JSDTTernCompletionProposal extends JSTernCompletionProposal
 		implements IJavaCompletionProposal {
 
+	private static int CATEGORY_SEPARATION = 100000;
+
+	private static int CAT_ORDER_PRIMITIVE_FIELD = 1;
+	private static int CAT_ORDER_OBJECT_FIELD = 2;
+	private static int CAT_ORDER_FUNCTION = 3;
+	private static int CAT_ORDER_PRIMITIVE_STATIC = 4;
+	private static int CAT_ORDER_OBJECT_STATIC = 5;
+	private static int CAT_ORDER_KEYWORD = 6;
+	
 	private int relevance;
 	
 	@Deprecated
@@ -35,11 +44,49 @@ public class JSDTTernCompletionProposal extends JSTernCompletionProposal
 			String url, String origin, boolean keyword, int depth, 
 			int pos, int startOffset) {
 		super(name, type, doc, url, origin, keyword, pos, startOffset);
-		if (keyword) {
-			relevance = 0;
-		} else {
-			relevance = 1000000 - depth;
+		relevance = (10 - getCategory()) * CATEGORY_SEPARATION - depth;
+	}
+	
+	private int getCategory() {
+		if (isKeyword()) {
+			return CAT_ORDER_KEYWORD;
 		}
+		if (isFunction()) {
+			return CAT_ORDER_FUNCTION;
+		}
+
+		
+		boolean allUpperCase = true;
+		String name = getName();
+		for (int i = 0; i < name.length(); i++) {
+			if (Character.isLowerCase(name.charAt(i))) {
+				allUpperCase = false;
+				break;
+			}
+		}
+		
+		String type = getType();
+		boolean object = !(
+				"bool".equals(type) || 
+				"number".equals(type) || 
+				"string".equals(type));
+		
+		int result;
+		if (allUpperCase) {
+			if (object) {
+				result = CAT_ORDER_OBJECT_STATIC;
+			} else {
+				result = CAT_ORDER_PRIMITIVE_STATIC;
+			} 
+		} else {
+			if (object) {
+				result = CAT_ORDER_OBJECT_FIELD;
+			} else {
+				result = CAT_ORDER_PRIMITIVE_FIELD;
+			}
+		}
+		
+		return result;
 	}
 
 	@Override
