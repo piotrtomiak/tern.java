e2f21dcfa8c10bdb3cc5428e66a91fe25e17393f
Piotr Tomiak <piotr@genuitec.com>
2015-12-28 13:43:30 +0100

    [nobug] Provide support for call hierarchy.
    Change-Id: I632bcecbded20ae6a71badf78b4640f394b9ee92

--- core/tern.core/src/tern/server/protocol/refs/TernRefsQuery.java
+++ core/tern.core/src/tern/server/protocol/refs/TernRefsQuery.java
@@ -13,9 +13,16 @@ public class TernRefsQuery extends TernQuery {
 	private static final String REFS_TYPE_QUERY = "refs";
 
 	public TernRefsQuery(String file, Integer pos) {
+		this(file, pos, null);
+	}
+	
+	public TernRefsQuery(String file, Integer pos, Boolean onlySourceFile) {
 		super(REFS_TYPE_QUERY);
 		setFile(file);
 		setEnd(pos);
+		if (onlySourceFile != null) {
+			set("onlySourceFile", onlySourceFile); //$NON-NLS-1$
+		}
 	}
 
 }
