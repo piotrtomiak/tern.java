b32c8091a40fbb727fe2cfb67cd5b0e576d73d08
Wojciech Galanciak <wojtek@genuitec.com>
2016-04-20 12:47:48 +0200

    [29641] [JS][ES6] Improve outline
    Change-Id: I3c940f000ce36eb06012fef99e0c384e9158017b

--- webclipse/ternjs.webclipse/dist/webclipse.js
+++ webclipse/ternjs.webclipse/dist/webclipse.js
@@ -323,51 +323,104 @@ var _ternLibTern = _dereq_("tern/lib/tern");
 
 var tern = _interopRequireWildcard(_ternLibTern);
 
-function processProperty(server, file, prop, parent, level) {
-  if (prop.origin) {
-    var propFile = server.fileMap[prop.origin];
-    if (propFile) {
-      var child = {
+var _ternLibInfer = _dereq_("tern/lib/infer");
+
+var infer = _interopRequireWildcard(_ternLibInfer);
+
+function processProperty(server, file, prop, parent, level, resolvedTypes) {
+  var originFile;
+  if (prop.originNode && prop.originNode.sourceFile) {
+    originFile = prop.originNode.sourceFile.name;
+  } else {
+    originFile = prop.origin;
+  }
+  if (!originFile || !server.fileMap[originFile] || level == 1 && originFile != file.name) {
+    return;
+  }
+  var child;
+  // create child node only if this is not a prototype
+  if (prop.propertyName != "prototype") {
+    var nodeType = prop.types.length > 0 ? prop.types.join('|') : "?";
+    if (!parent.name || parent.kind == "prototype" || nodeType.startsWith("fn(")) {
+      child = {
         name: prop.propertyName,
-        file: propFile.name,
-        value: propFile.name != file.name,
-        type: prop.types.length > 0 ? prop.types.join('|') : "?",
+        file: originFile,
+        value: originFile != file.name,
+        type: nodeType,
         start: prop.originNode ? prop.originNode.start : -1,
         end: prop.originNode ? prop.originNode.end : -1
       };
-      for (var typeNr in prop.types) {
-        var type = prop.types[typeNr];
-        gather(server, file, type, child, level);
-      }
-
-      var children = parent;
-      if (!(children instanceof Array)) {
-        if (children.children) {
-          children = children.children;
-        } else {
-          children = children.children = [];
+    }
+  } else {
+    // mark the parent as a prototype
+    parent.kind = "prototype";
+  }
+  // process property only if it is not a constructor
+  if (prop.propertyName != "constructor") {
+    for (var typeNr in prop.types) {
+      var type = prop.types[typeNr];
+      if (type) {
+        if (!child || child.type.startsWith("fn(") || resolvedTypes.indexOf(type.name) == -1) {
+          if (parent && parent.kind == "prototype" && resolvedTypes.indexOf(parent.name) == -1) {
+            resolvedTypes.push(parent.name);
+          }
+          gather(server, file, type, child ? child : parent, level, resolvedTypes);
+          if (type.originNode && (type.originNode.type == "FunctionExpression" || type.originNode.type == "FunctionDeclaration")) {
+            var scope = infer.scopeAt(file.ast, type.originNode.end);
+            if (scope && (scope.isBlock || scope.fnType)) {
+              gather(server, file, scope, child ? child : parent, level, resolvedTypes);
+            }
+          }
         }
       }
-      children.push(child);
     }
   }
+  if (child) {
+    var children = parent;
+    if (!(children instanceof Array)) {
+      if (children.children) {
+        children = children.children;
+      } else {
+        children = children.children = [];
+      }
+    }
+    for (var c in children) {
+      if (child.name == children[c].name) {
+        return;
+      }
+    }
+    children.push(child);
+  }
 }
 
-function gather(server, file, node, parent, level) {
-  if (level++ > 10) return;
+function gather(server, file, node, parent, level, resolvedTypes) {
+  if (level++ > 6) return;
+  if (node.instances && node.instances.length > 0) {
+    var instance = node.instances[0].instance;
+    if (instance) {
+      for (var propNr in instance.props) {
+        processProperty(server, file, instance.props[propNr], parent, level, resolvedTypes);
+      }
+    }
+  }
   for (var propName in node.props) {
-    processProperty(server, file, node.props[propName], parent, level);
+    var prop = node.props[propName];
+    if (node.fnType && node.fnType.argNames.indexOf(prop.propertyName) != -1) {
+      continue;
+    }
+    processProperty(server, file, prop, parent, level, resolvedTypes);
   }
-  for (var propName in node.maybeProps) {
-    processProperty(server, file, node.maybeProps[propName], parent, level);
+  if (node.proto && (node.proto.name == parent.name || node.proto.name == node.name + ".prototype")) {
+    gather(server, file, node.proto, parent, --level, resolvedTypes);
   }
 }
 
-function create(server, query, file) {
+function create(server, query, file, resolvedTypes) {
   try {
     var outline = [],
         scope = file.scope;
-    gather(server, file, scope, outline, 0);
+    var resolvedTypes = ["boolean", "number", "string", "symbol", "Array"];
+    gather(server, file, scope, outline, 0, resolvedTypes);
     return { outline: outline };
   } catch (err) {
     console.error(err, err.stack);
@@ -375,7 +428,7 @@ function create(server, query, file) {
   }
 }
 
-},{"tern/lib/tern":undefined}],3:[function(_dereq_,module,exports){
+},{"tern/lib/infer":undefined,"tern/lib/tern":undefined}],3:[function(_dereq_,module,exports){
 "use strict";
 
 exports.__esModule = true;
